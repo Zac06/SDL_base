9314cc42f5b239753a57b3c06572163fca5e5fad
Zaccaria <zaccaria.francescato@gmail.com>
2024-06-07 22:07:22 +0200

    added doxigen to gui_button

--- include/gui_elements/gui_button.hpp
+++ include/gui_elements/gui_button.hpp
@@ -11,10 +11,29 @@ using namespace std;
 
 class gui_button : public gui_element{
     public:
+        /// @brief Shortest constructor.
+        /// @param p_tex_normal Texture at a normal state.
+        /// @param p_events Main event manager.
+        /// @param p_coords X and Y coordinates of the texture.
         gui_button(SDL_Texture* p_tex_normal, event_mgr& p_events, SDL_Point p_coords);
+
+        /// @brief Mid-length constructor.
+        /// @param p_tex_normal Texture in a normal state.
+        /// @param p_tex_active Texture in an active state (while cursor is over and mouse pressed)
+        /// @param p_events Main event manager.
+        /// @param p_coords X and Y coordinates os the textures.
         gui_button(SDL_Texture* p_tex_normal, SDL_Texture* p_tex_active, event_mgr& p_events, SDL_Point p_coords);
+
+        /// @brief Full-length construtor.
+        /// @param p_tex_normal Texture in a normal state.
+        /// @param p_tex_hover Texture in a hovered state (when the cursor is over it)
+        /// @param p_tex_active Texture in an active state (while cursor is over and mouse pressed)
+        /// @param p_tex_disabled Texture in a disabled state
+        /// @param p_events Main event manager.
+        /// @param p_coords X and Y coordinates of the texture.
         gui_button(SDL_Texture* p_tex_normal, SDL_Texture* p_tex_hover, SDL_Texture* p_tex_active, SDL_Texture* p_tex_disabled, event_mgr& p_events, SDL_Point p_coords);
 
+        /// @brief Checks for new states to become.
         void update() override;
 
     private:
